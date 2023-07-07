05eb8bc72e92c8bfc74713a29699e0c3b9fbc53c
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-07 22:19:31 +0700

    Create Telco.php

---
+++ src/hachkingtohach1/NapThe/Telco.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace hachkingtohach1\NapThe;
+
+final class Telco {
+
+	public const VIETTEL = "VIETTEL";
+
+	public const VINA = "VINA";
+
+	public const MOBI = "MOBI";
+
+	public const VIETNAMMOBI = "VIETNAMMOBI";
+
+	public const ZING = "ZING";
+
+	public const GARENA = "GARENA";
+
+	public const VCOIN = "VCOIN";
+
+	public const GATE = "GATE";
+}
