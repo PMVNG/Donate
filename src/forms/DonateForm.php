102e6913aca1640e58230fd374e981d9a7a95218
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-08 14:48:34 +0700

    Add field

--- src/forms/DonateForm.php
+++ src/forms/DonateForm.php
@@ -7,6 +7,8 @@ namespace Donate\forms;
 use dktapps\pmforms\CustomForm;
 use dktapps\pmforms\CustomFormResponse;
 use dktapps\pmforms\element\Dropdown;
+use dktapps\pmforms\element\Input;
+use dktapps\pmforms\element\StepSlider;
 use Donate\Constant;
 use pocketmine\player\Player;
 
@@ -16,7 +18,10 @@ class DonateForm {
 		return new CustomForm(
 			title: "Biểu Mẫu Nạp Thẻ",
 			elements: [
-				new Dropdown("telco", text: "Loại thẻ", options: Constant::TELCO)
+				new Dropdown(name: "telco", text: "Loại thẻ", options: Constant::TELCO),
+				new StepSlider(name: "amount", text: "Mệnh giá", options: Constant::AMOUNT),
+				new Input(name: "serial", text: "Số sê-ri", hintText: "Nhập số sê-ri tại đây:\nVí dụ: 10004783347874"),
+				new Input(name: "code", text: "Mã thẻ", hintText: "Nhập mã thẻ tại đây:\nVí dụ: 312821445892982")
 			],
 			onSubmit: function(Player $submitter, CustomFormResponse $response) : void{
 				var_dump($response);
