b3c24a8aac6e604e88ec4ed9a4305dc824df5286
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-08 14:49:52 +0700

    Reformat

--- src/forms/DonateForm.php
+++ src/forms/DonateForm.php
@@ -18,12 +18,28 @@ class DonateForm {
 		return new CustomForm(
 			title: "Biểu Mẫu Nạp Thẻ",
 			elements: [
-				new Dropdown(name: "telco", text: "Loại thẻ", options: Constant::TELCO),
-				new StepSlider(name: "amount", text: "Mệnh giá", options: Constant::AMOUNT),
-				new Input(name: "serial", text: "Số sê-ri", hintText: "Nhập số sê-ri tại đây:\nVí dụ: 10004783347874"),
-				new Input(name: "code", text: "Mã thẻ", hintText: "Nhập mã thẻ tại đây:\nVí dụ: 312821445892982")
+				new Dropdown(
+					name: "telco",
+					text: "Loại thẻ",
+					options: Constant::TELCO
+				),
+				new StepSlider(
+					name: "amount",
+					text: "Mệnh giá",
+					options: Constant::AMOUNT
+				),
+				new Input(
+					name: "serial",
+					text: "Số sê-ri",
+					hintText: "Nhập số sê-ri tại đây:\nVí dụ: 10004783347874"
+				),
+				new Input(
+					name: "code",
+					text: "Mã thẻ",
+					hintText: "Nhập mã thẻ tại đây:\nVí dụ: 312821445892982"
+				)
 			],
-			onSubmit: function(Player $submitter, CustomFormResponse $response) : void{
+			onSubmit: function (Player $submitter, CustomFormResponse $response): void {
 				var_dump($response);
 			},
 		);
