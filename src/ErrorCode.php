df2bac07a70400c49a332a84c40b8519897eddc4
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-09 12:33:53 +0700

    Create ErrorCode.php

---
+++ src/ErrorCode.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Donate;
+
+class ErrorCode {
+
+	public const AZ001 = " [Error Code: AZ001]";
+
+	public const AZ002 = " [Error Code: AZ002]";
+
+	public const AZ003 = " [Error Code: AZ003]";
+
+	public const AZ004 = " [Error Code: AZ004]";
+
+	public const AZ005 = " [Error Code: AZ005]";
+
+	public const AZ006 = " [Error Code: AZ006]";
+
+	public const AZ007 = " [Error Code: AZ007]";
+
+	public const AZ008 = " [Error Code: AZ008]";
+
+	public const AZ009 = " [Error Code: AZ009]";
+
+	public const AZ010 = " [Error Code: AZ010]";
+
+	public const AZ011 = " [Error Code: AZ011]";
+
+	public const AZ012 = " [Error Code: AZ012]";
+
+	public const AZ013 = " [Error Code: AZ013]";
+
+	public const AZ014 = " [Error Code: AZ014]";
+
+	public const AZ015 = " [Error Code: AZ015]";
+
+	public const AZ016 = " [Error Code: AZ016]";
+
+	public const AZ017 = " [Error Code: AZ017]";
+
+	public const AZ018 = " [Error Code: AZ018]";
+
+	public const AZ019 = " [Error Code: AZ019]";
+
+	public const AZ020 = " [Error Code: AZ020]";
+
+	public const AZ021 = " [Error Code: AZ021]";
+
+	public const AZ022 = " [Error Code: AZ022]";
+
+	public const AZ023 = " [Error Code: AZ023]";
+
+	public const AZ024 = " [Error Code: AZ024]";
+
+	public const AZ025 = " [Error Code: AZ025]";
+
+	public const AZ026 = " [Error Code: AZ026]";
+
+	public const AZ027 = " [Error Code: AZ027]";
+
+	public const AZ028 = " [Error Code: AZ028]";
+
+	public const AZ029 = " [Error Code: AZ029]";
+
+	public const AZ030 = " [Error Code: AZ030]";
+
+}
